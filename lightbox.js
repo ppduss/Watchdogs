4e1e6aaed81bbdac31d194f1bdac4e9d35a14628
Patrick <ppduss@gmail.com>
2020-12-04 11:32:43 -0500

    first

---
+++ lightbox.js
@@ -0,0 +1,8 @@
+
+export function openLightbox() {
+  document.getElementById("myLightbox").style.display = "block";
+}
+
+export function closeLightbox() {
+  document.getElementById("myLightbox").style.display = "none";
+}
